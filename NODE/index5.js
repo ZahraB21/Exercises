a8810d021a9eb75c0d42eabf46aa29b6d141671c
Zahra <zbehfarshad@gmail.com>
2020-01-22 20:02:24 -0800

    Exercise 5 Completed

---
+++ NODE/index5.js
@@ -0,0 +1,11 @@
+var firstName = "Jane";
+var lastName = "Doe";
+var age = 20;
+var dob = '20/12/1990';
+var address = "Vancouver";
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Age: " + age)
+console.log("Date of Birth: " + dob);
+console.log("Address: " + address);
