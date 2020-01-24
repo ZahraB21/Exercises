db2067cf11079ecceb924996bb257f7e1063ee5d
Zahra <zbehfarshad@gmail.com>
2020-01-24 12:31:43 -0800

    Exercise 26 Completed

---
+++ NODE/index26.js
@@ -0,0 +1,24 @@
+let city = 'Winnipeg';
+let capital = 'Manitoba';
+let hearts = 'The Forks';
+let country = true;
+let cold = true;
+let population = 749534;
+let rivers = null;
+
+console.log(`City: ${city}
+Capital: ${capital}
+Heart of: ${hearts}
+Country: ${country}
+Cold: ${cold}
+Population: ${population}
+rivers: ${rivers}
+`);
+
+console.log(`Type of city: ` + typeof(city));
+console.log(`Type of capital: ` + typeof(capital));
+console.log(`Type of country: ` + typeof(country));
+console.log(`Type of hearts: ` + typeof(hearts));
+console.log(`Type of cold: ` + typeof(cold));
+console.log(`Type of rivers: ` + typeof(rivers));
+console.log(`Type of population: ` + typeof(population));
