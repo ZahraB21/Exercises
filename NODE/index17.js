2d9f9a207c7cfb819e39bce9dc7d4799c6d4407d
Zahra <zbehfarshad@gmail.com>
2020-01-24 10:49:29 -0800

    Exercise 17 Completed

---
+++ NODE/index17.js
@@ -0,0 +1,6 @@
+let seriesName = 'Game Of Thrones';
+let dragon1 = 'Drogan';
+let dragon2 = 'Rhaegal';
+let dragon3 = 'Viserion';
+let templateLiteral = `In ${seriesName}, the dragon names are: ${dragon1}, ${dragon2}, ${dragon3}`;
+console.log(templateLiteral);
