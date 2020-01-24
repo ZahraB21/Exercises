801dfece7d174be7868b79ba7261c3c6721de9da
Zahra <zbehfarshad@gmail.com>
2020-01-24 12:02:42 -0800

    Exercise 21 Completed

---
+++ NODE/index21.js
@@ -0,0 +1,6 @@
+let age = 30;
+let phone = 7783889182;
+let streetName = '63rd Avenue';
+let streetNumber = '1210';
+
+console.log(`I'm ${age} years old. My phone number is: ${phone}. I live on ${streetNumber} ${streetName}`);
