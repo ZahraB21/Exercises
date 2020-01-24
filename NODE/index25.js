2ffa56e8f3d1d3aff25c3e7ca5985ee882f88bb1
Zahra <zbehfarshad@gmail.com>
2020-01-24 12:22:21 -0800

    Exercise 25 Completed

---
+++ NODE/index25.js
@@ -0,0 +1,16 @@
+let superHeroName = 'Batman';
+let name = 'Bruce Wayne';
+let fly = false;
+let hasBatMobile = true;
+let life = 35;
+let freeTime = null;
+
+console.log(`Super Hero: ${superHeroName} \nName: ${name} \nFly: ${fly} \nHas Bat Mobile: ${hasBatMobile}\nLife: ${life}
+Free Time: ${freeTime}`);
+
+console.log(`Type of superHeroName: ` + typeof(superHeroName));
+console.log(`Type of name: ` + typeof(name));
+console.log(`Type of fly: ` + typeof(fly));
+console.log(`Type of hasBatMobile: ` + typeof(hasBatMobile));
+console.log(`Type of life: ` + typeof(life));
+console.log(`Type of freeTime: ` + typeof(freeTime));
