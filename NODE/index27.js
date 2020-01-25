ae2835571e1ac58da7fb3751b3a058e5277e90be
Zahra <zbehfarshad@gmail.com>
2020-01-24 19:57:50 -0800

    Exercise 27 Completed

---
+++ NODE/index27.js
@@ -0,0 +1,9 @@
+let firstNumber = 26;
+let secondNumber = 21;
+
+console.log(`Adding ${firstNumber} + ${secondNumber} equals to: ` + (firstNumber + secondNumber));
+console.log(`Subtracting ${firstNumber} - ${secondNumber} equals to: ` + (firstNumber - secondNumber));
+console.log(`Multiplying ${firstNumber} * ${secondNumber} equals to: ` + (firstNumber * secondNumber));
+console.log(`Dividing ${firstNumber} / ${secondNumber} equals to: ` + (firstNumber / secondNumber));
+console.log(`Adding ${firstNumber} % ${secondNumber} equals to: ` + (firstNumber % secondNumber));
+
