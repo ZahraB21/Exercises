ae0107988599dd592c8b01ce26c5a36ebfebf6a5
Zahra <zbehfarshad@gmail.com>
2020-01-24 20:29:00 -0800

    Exercise 32 Completed

---
+++ NODE/index32.js
@@ -0,0 +1,12 @@
+let height = 10;
+let base = 4;
+
+let perimiter = (height + base) * 2;
+let area = height * base;
+
+console.log(`Height: ${height} 
+Base: ${base}
+Perimeter: ${perimiter}
+Area: ${area}`
+);
+
