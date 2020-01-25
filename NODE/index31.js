def4117f2f455094ba3a2e3e3f238aeb7e3d3e52
Zahra <zbehfarshad@gmail.com>
2020-01-24 20:12:55 -0800

    Exercise 31 Completed

---
+++ NODE/index31.js
@@ -0,0 +1,6 @@
+let side1 = 10;
+let side2 = 20;
+let side3 = 5;
+let perimeter = side1 + side2 + side3;
+
+console.log(`Side1 length is: ${side1}\nSide2 length is: ${side2}\nSide3 length is: ${side3}\nTriangle Perimeter: ${perimeter}`);
