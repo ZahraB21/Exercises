17f76410a0b9d8b005c8d8373380d3f0a2bafd74
Zahra <zbehfarshad@gmail.com>
2020-02-05 14:52:20 -0800

    Exercise 60 Completed

---
+++ NODE/index60.js
@@ -0,0 +1,9 @@
+let grade = 6;
+
+if(grade >= 9){
+    console.log(`Great Work!`);
+} else if( grade >= 6 && grade < 9){
+    console.log(`You've done it!`);
+} else{
+    console.log(`You need to do all the exercises again, please try again!`);
+}
