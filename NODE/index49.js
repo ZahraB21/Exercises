8b12b3c139c7e8c1e9f020ac3ce86f5c66927888
Zahra <zbehfarshad@gmail.com>
2020-02-05 14:47:50 -0800

    Exercise 49 Completed

---
+++ NODE/index49.js
@@ -0,0 +1,7 @@
+let firstNumber = 10;
+let secondNumber = 5;
+
+if(firstNumber > secondNumber)
+    console.log(`${firstNumber} is greater than ${secondNumber}`);
+else
+console.log(`${secondNumber} is greater than ${firstNumber}`);
