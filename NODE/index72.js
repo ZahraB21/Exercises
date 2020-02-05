89ab0aa5e83bde3cc9cf293a9b25a45f2d933219
Zahra <zbehfarshad@gmail.com>
2020-02-05 15:10:19 -0800

    Exercise 72 Using the repeat function to create the stars

--- NODE/index72.js
+++ NODE/index72.js
@@ -1,14 +1,20 @@
 let counter = 1;
 
+// while(counter <= 10){
+//     let j = 1;
+//     let stars = '*';
+//     while(j <= counter){
+//         if( counter === 1 )
+//             console.log(`${stars}`);
+//         stars = stars.concat('*');
+//         j++;
+//     }
+//     console.log(`${stars}`);
+//     counter++;
+// }
+
 while(counter <= 10){
-    let j = 1;
-    let stars = '*';
-    while(j <= counter){
-        if( counter === 1 )
-            console.log(`${stars}`);
-        stars = stars.concat('*');
-        j++;
-    }
-    console.log(`${stars}`);
+
+    console.log('*'.repeat(counter));
     counter++;
 }
