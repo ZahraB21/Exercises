d8f73644446f91abfa8e4f156a83144d04950725
Zahra <zbehfarshad@gmail.com>
2020-02-05 17:57:47 -0800

    Exercise 130 Completed

---
+++ NODE/index130.js
@@ -0,0 +1,11 @@
+const mutants = ['Professor X','Cyclops','Angel','Beast','Phoenix'];
+console.log(`Original List: ${mutants}`);
+
+const sortedList = mutants.sort();
+console.log(`Sorted List: ${sortedList}`);
+
+const reversedList = mutants.reverse();
+console.log(`Reversed List: ${reversedList}`);
+
+const joinedList = mutants.join(' * ');
+console.log(`Joined List: ${joinedList}`);
