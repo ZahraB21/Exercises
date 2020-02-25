c86233bb9144bb4025413418ac8de354bf36aef4
Zahra <zbehfarshad@gmail.com>
2020-02-24 20:12:46 -0800

    Exercise 7 Completed

---
+++ NPM/Ex_7/greeter.js
@@ -0,0 +1,8 @@
+const greet = (firstName, lastName, func) => {
+  console.log(`Hi ${firstName} ${lastName}`);
+  func();
+};
+
+module.exports = {
+  greet
+};
---
+++ NPM/Ex_7/index.js
@@ -0,0 +1,5 @@
+const greeter = require("./greeter");
+
+greeter.greet("Zahra", "Behfarshad", () =>
+  console.log("We're using node modules yeah!!!")
+);
